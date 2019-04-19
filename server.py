ab870b666347435acf445f2f93a1f280d197dfce
Evgeny Ovchinnikov <e.ovchinnikov@sysolutions.ru>
2019-04-19 16:55:51 +0700

    Initial commit

---
+++ server.py
@@ -0,0 +1,64 @@
+import socket
+from _thread import start_new_thread
+from player import Player
+import pickle
+
+
+server = "127.0.0.1"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+# maximum connections allowed
+s.listen(2)
+print("Waiting for connection, Server Started")
+
+
+
+
+players = [Player(0, 0, 50, 50, (255,0,0)), Player(100, 100, 50, 50, (0, 0, 255))]
+
+def threaded_client(conn, player):
+    conn.send(pickle.dumps(players[player]))
+    reply = ""
+    # must run while we have connected clients
+    while True:
+        try:
+            data = pickle.loads(conn.recv(2048))
+
+            # обновляем текующие координаты объекта player
+            players[player] = data
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                if player == 1:
+                    reply = players[0]
+                else:
+                    reply = players[1]
+                print("Received: ", data)
+                print("Sending: ", reply)
+
+            # отправка игрокам новых координат
+            conn.sendall(pickle.dumps(reply))
+        except:
+            break
+
+    print("Lost connection")
+    conn.close()
+
+
+currentPlayer = 0
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
